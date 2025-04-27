fcf190bc49017d3db3a070312e76def78fda43c5
Taj <taj.nijjar2002@gmail.com>
2025-04-27 23:01:43 +1200

    Fixed CROS issue and added POST.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,27 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		if (request.method === 'OPTIONS') {
+			// Handle CORS preflight request
+			return new Response(null, {
+				status: 204,
+				headers: {
+					'Access-Control-Allow-Origin': 'https://religious-wonders.pages.dev/',
+					'Access-Control-Allow-Methods': 'POST, OPTIONS, GET',
+					'Access-Control-Allow-Headers': 'Content-Type',
+				},
+			});
+		}
+		if (request.method === 'POST') {
+			const body = await request.json();
+			const option = body.option;
+
+			console.log('Vote received for:', option);
+
+			// TODO: Save to a storage backend like KV or external DB
+			return new Response('Vote received: ' + option, { status: 200 });
+		}
+
+		return new Response('Use POST to submit vote', { status: 405 });
 	},
 };
